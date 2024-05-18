7345783d8d560763c4f9d31b186a1a52bce46f80
Gonzalo <gonzaescobar23@gmail.com>
2024-05-18 00:12:57 -0300

    Agregados: rutas Chi y middleware.

--- main.go
+++ main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+
+	router.Get("/hola", basicHandler)
+
+
 	server := &http.Server{
 		Addr: ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 	
 	fmt.Println("Servidor: online.\nHost: http://localhost:3000")
